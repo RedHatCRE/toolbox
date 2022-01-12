413955bd58766fd85a47423ee525aa8f731177a2
Adrian Fusco Arnejo <afuscoar@redhat.com>
2022-01-12 16:05:38 +0100

    - Added new required parameter 'ssh_user' - ssh_key_path now is optional - Commands has been grouped in arrays. If the ssh_key_path is provided now is appended. - Change the jq command to avoid null and get the comments correctly - Changes for pass the shellcheck test - Changed how to use script message and README

--- get_gerrit_last_comment_by_data.sh
+++ get_gerrit_last_comment_by_data.sh
@@ -6,11 +6,11 @@ if ! type jq >/dev/null 2>&1; then
 fi
 
 usage() { 
-    echo "Usage: $0 [-H|--gerrit_host HOST] [-p|--port NUM] [-c|--change_number NUM] [-s|--ssh_key_path PATH] [-u|--gerrit_username USERNAME]"
+    echo "Usage: $0 [-u|--ssh_user USERNAME] [-p|--port NUM] [-s|--ssh_key_path PATH] [-H|--gerrit_host HOST] [-U|--gerrit_username USERNAME] [-C|--change_number NUM]"
     exit 2
 }
 
-declare gerrit_host port change_number ssh_key_path gerrit_username
+declare gerrit_host port change_number ssh_key_path gerrit_username review_data
 
 while [ $# -gt 0 ]; do
     case $1 in
@@ -18,22 +18,26 @@ while [ $# -gt 0 ]; do
             gerrit_host=$2
             shift
             ;;
+        -U|--gerrit_username)
+            gerrit_username=$2
+            shift
+            ;;
+        -C|--change_number) 
+            change_number=$2
+            shift
+            ;;
         -p|--port) 
             port=$2
             shift
             ;;
-        -c|--change_number) 
-            change_number=$2
+        -u|--ssh_user)
+            ssh_user=$2
             shift
             ;;
         -s|--ssh_key_path)
             ssh_key_path=$2
             shift
-            ;;
-        -u|--gerrit_username)
-            gerrit_username=$2
-            shift
-            ;;
+            ;;    
         --)
             shift
             break
@@ -44,6 +48,11 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+if [ -z "$ssh_user" ]; then
+    echo 'Missing -u parameter: --ssh_user'
+    exit 2
+fi
+
 if [[ -z $port ]]; then
     port=22
 fi
@@ -53,42 +62,47 @@ if [ -z "$gerrit_host" ]; then
     exit 2
 fi
 
-if [ -z "$change_number" ]; then
-    echo 'Missing -c parameter: --change_number'
-    exit 2
-fi
-
-if [ -z "$ssh_key_path" ]; then
-    echo 'Missing -s parameter: --ssh_key_path'
-    exit 2
-fi
-
 if [ -z "$gerrit_username" ]; then
-    echo 'Missing -u parameter: --gerrit_username'
+    echo 'Missing -U parameter: --gerrit_username'
     exit 2
 fi
 
-review_data=$(ssh -p "$port" "$gerrit_host" gerrit query --comments --current-patch-set change:"$change_number" --format=json commentby:"$gerrit_username" --format=JSON)
+if [ -z "$change_number" ]; then
+    echo 'Missing -C parameter: --change_number'
+    exit 2
+fi
 
-# Gerrit returns error
-if jq -e 'select(.type=="error")' <<< "$review_data"
+ssh_command=("ssh" "-p" "$port" "$ssh_user@$gerrit_host")
+gerrit_command=( "gerrit" "query" "--comments" "change:$change_number" "--format=json" "commentby:$gerrit_username" "--format=JSON")
+
+if [ ! -z "$ssh_key_path" ]; then
+    if [ ! -f "$ssh_key_path" ]; then
+        echo "SSH key '$ssh_key_path' not found"
+        exit 2
+    fi
+    ssh_command+=('-i' $ssh_key_path)
+fi
+
+review_data=$("${ssh_command[@]}" "${gerrit_command[@]}")
+
+if jq -e 'select(.type=="error")' <<< "${review_data[@]}"
 then
+    echo "An error has been returned by Gerrit"
     exit 1
 fi
 
-# There is no data
-if jq -e 'select(.rowCount==0)' <<< "$review_data"
+if jq -e 'select(.rowCount==0)' <<< "${review_data[@]}"
 then
+    echo "There is no data found in the review"
     exit 1
 fi
 
-comment_rows=$(echo "$review_data" | head -n1)
+comment_rows=$(echo "${review_data[@]}" | head -n1)
 
-jq -r --raw-output --exit-status "
-                                 [.comments 
-                                 | .[] 
-                                 | select(.reviewer.username==\"$gerrit_username\")] 
-                                 | last.message" <<< "$comment_rows" \
+echo "$comment_rows" | jq -r --raw-output --exit-status "
+                                                      .comments 
+                                                      | .[]?
+                                                      | select(.reviewer.username==\"$gerrit_username\")
+                                                      | .message" \
 | awk '/^-/{print $2,$3,$5}' \
 | column --table --table-columns TEST_NAME,URL,STATUS --json --table-name data
-
