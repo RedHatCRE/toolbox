a403ac518ff348d0ff05cb241597a0ec7714272e
Adrian <59370927+adrianfusco@users.noreply.github.com>
2022-01-25 11:38:55 +0100

    New gerrit script, tox test file and pipeline (#1)
    * New script, tox test file and pipeline
    
    - Added new script 'get_gerrit_last_comment_by_id.sh' to get the last comment of a gerrit revision.
    - Added new tox.ini to test with flake8 python scripts.
    - Added new Github workflow to test all python and bash scripts.
    
    * Separate the commands to exit if SSH is not successful.
    
    * Add newline at end of github workflow
    
    * Add newline at end of script
    
    * Review github workflow
    
    - Separate multiline commands in single task
    - Delete quiet option
    
    * Delete external whitelist directive in tox file
    
    * Script reviews
    
    - One liners deleted.
    - Deleted unnecessary semicolon
    - Added usage info.
    
    * Change magic operator for :- expansion parameter
    
    * Update README.md
    
    - Changed ID by change_number to avoid confusion between this one and the change ID
    
    * Update get_gerrit_last_comment_by_id.sh
    
    - Changed ID by change_number to avoid confusion between this one and the change ID
    
    * Update README.md
    
    - Update usage info
    
    * Update script to get gerrit coments
    
    - Script name changed.
    - Added username parameter. It is used to search by username too not just change number.
    - Added error handle depending of the gerrit message returned.
    
    * Review for shellcheck correction
    
    * Parse first json that has the comments
    
    * Add tox.ini flake8 deps
    
    * Commit new files
    
    * Fix conditional to required username parameter
    
    * Parse script for zuul data
    
    * Change gerrit host parameter -h into -H for distinguish help parameter
    
    * Changed command into type to avoid the execution of the command and check if it's in the PATH
    
    * - Added new required parameter 'ssh_user'
    - ssh_key_path now is optional
    - Commands has been grouped in arrays. If the ssh_key_path is provided now is appended.
    - Change the jq command to avoid null and get the comments correctly
    - Changes for pass the shellcheck test
    - Changed how to use script message and README
    
    * - Changed echo to herestring. It's a little bit faster.
    - Added a '--get_failure_jobs' option. With this we can extract just the jobs with result FAILURE.
    - Added a new output format with jq to return a json.
    
    * Delete parameter that already exist
    
    * Fix shellcheck test error for single quotes
    
    Change-Id: Idc256525b8d6ec71d708980b3dd294384930ab04

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cre
+
 Code Reliability Engineering Toolbox
+
+## Get the last comment in Gerrit by Data
+
+This tool is used to extract the information in the last comment of a Gerrit revision. We should pass the server where gerrit is hosted, the gerrit change number of the revision, the gerrit username and the path of the SSH key to connect to the given server.
+
+`jq` command is required to parse the JSON reply of the gerrit server.
+
+Usage:
+
+```
+$ ./get_gerrit_last_comment_by_data.sh --help
+Usage: ./get_gerrit_last_comment_by_data.sh [-u|--ssh_user USERNAME] [-p|--port NUM] [-s|--ssh_key_path PATH] [-H|--gerrit_host HOST] [-U|--gerrit_username USERNAME] [-C|--change_number NUM] [-g|--get-failure-jobs]
+```
+
+
---
+++ get_gerrit_last_comment_by_data.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+
+if ! type jq >/dev/null 2>&1; then
+    echo >&2 "I require jq but it's not installed. Aborting."
+    exit 1
+fi
+
+usage() { 
+    echo "Usage: $0 [-u|--ssh_user USERNAME] [-p|--port NUM] [-s|--ssh_key_path PATH] [-H|--gerrit_host HOST] [-U|--gerrit_username USERNAME] [-C|--change_number NUM] [-g|--get-failure-jobs]"
+    exit 2
+}
+
+declare gerrit_host port change_number ssh_key_path gerrit_username review_data get_failure_jobs
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -H|--gerrit_host)
+            gerrit_host=$2
+            shift
+            ;;
+        -U|--gerrit_username)
+            gerrit_username=$2
+            shift
+            ;;
+        -C|--change_number) 
+            change_number=$2
+            shift
+            ;;
+        -p|--port) 
+            port=$2
+            shift
+            ;;
+        -u|--ssh_user)
+            ssh_user=$2
+            shift
+            ;;
+        -s|--ssh_key_path)
+            ssh_key_path=$2
+            shift
+            ;;
+        -g|--get_failure_jobs)
+            get_failure_jobs=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            usage
+    esac
+    shift
+done
+
+if [ -z "$ssh_user" ]; then
+    echo 'Missing -u parameter: --ssh_user'
+    exit 2
+fi
+
+if [[ -z $port ]]; then
+    port=22
+fi
+
+if [ -z "$gerrit_host" ]; then
+    echo 'Missing -H parameter: --gerrit_host'
+    exit 2
+fi
+
+if [ -z "$gerrit_username" ]; then
+    echo 'Missing -U parameter: --gerrit_username'
+    exit 2
+fi
+
+if [ -z "$change_number" ]; then
+    echo 'Missing -C parameter: --change_number'
+    exit 2
+fi
+
+ssh_command=("ssh" "-p" "$port" "$ssh_user@$gerrit_host")
+gerrit_command=( "gerrit" "query" "--comments" "change:$change_number" "--format=json" "commentby:$gerrit_username")
+
+if [ ! -z "$ssh_key_path" ]; then
+    if [ ! -f "$ssh_key_path" ]; then
+        echo "SSH key '$ssh_key_path' not found"
+        exit 2
+    fi
+    ssh_command+=('-i' $ssh_key_path)
+fi
+
+review_data=$("${ssh_command[@]}" "${gerrit_command[@]}")
+
+if jq -e 'select(.type=="error")' <<< "${review_data[@]}"
+then
+    echo "An error has been returned by Gerrit"
+    exit 1
+fi
+
+if jq -e 'select(.rowCount==0)' <<< "${review_data[@]}"
+then
+    echo "There is no data found in the review"
+    exit 1
+fi
+
+comment_rows=$(head -n1 <<< "${review_data[@]}")
+
+last_comment_review_info=$(jq -r --raw-output --exit-status "
+                                                            [.project,
+                                                            [[.comments | .[] | select(.reviewer.username==\"$gerrit_username\")] | last | .message]
+                                                            ]" <<< "$comment_rows")
+
+project_name=$(jq -r first <<< "$last_comment_review_info")
+
+last_comment=$(jq -r 'last | .[]' <<< "$last_comment_review_info" \
+                      | awk '/^-/{print $2,$3,$5}')
+
+jq_extract_jobs_data_command=("jq" "-c" ".[]")
+if [ ! -z "$get_failure_jobs" ]; then
+    jq_extract_jobs_data_command=("jq" "-r" "'[.data | .[] | select(.status==\"FAILURE\")]'")
+fi
+
+data=$(echo "$last_comment" | column --table --table-columns TEST_NAME,URL,STATUS --json --table-name data | eval "${jq_extract_jobs_data_command[@]}") 
+
+jq -n --arg project_name "$project_name"  \
+      --arg change_id "$change_number"    \
+      --argjson data "$data"              \
+      "{project_name: \$project_name, change_id: \$change_id, data: \$data}"
+
